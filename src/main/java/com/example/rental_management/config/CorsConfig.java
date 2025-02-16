bd86d2eabc703a7ba43a1e259bf9b9fb26c9a894
Mậu Trường <115072922+truongnm-0478@users.noreply.github.com>
2025-02-16 23:29:25 +0700

    feat: Update CorsConfig.java

--- src/main/java/com/example/rental_management/config/CorsConfig.java
+++ src/main/java/com/example/rental_management/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://rental-management-fe-dd0g.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
